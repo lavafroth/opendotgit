0d2f8b6f300376ece61fc868447f6d33f5f7ff43
Himadri Bhattacharjee <lavafroth@protonmail.com>
2023-07-02 19:22:01 +0530

    refactor: separate ResponseExt trait into its own module

---
+++ src/response.rs
@@ -0,0 +1,46 @@
+use color_eyre::{eyre::bail, Result};
+use hyper::{
+    header::{CONTENT_LENGTH, CONTENT_TYPE},
+    Body, Response, StatusCode,
+};
+/// Adds extra functionality to `hyper::Response<Body>`.
+pub trait ResponseExt {
+    /// Returns true if the response has a `Content-Type` header indicating it is HTML.
+    fn is_html(&self) -> bool;
+
+    /// Verifies that the response is valid according to various criteria.
+    fn verify(&self) -> Result<()>;
+}
+
+impl ResponseExt for Response<Body> {
+    /// Returns true if the response has a `Content-Type` header indicating it is HTML.
+    fn is_html(&self) -> bool {
+        self.headers()
+            .get(CONTENT_TYPE)
+            .map(|content_type| {
+                content_type
+                    .to_str()
+                    .map(|t| t == "text/html")
+                    .unwrap_or(false)
+            })
+            .unwrap_or(false)
+    }
+
+    /// Verifies that the response is valid according to various criteria.
+    fn verify(&self) -> Result<()> {
+        let status = self.status();
+        if status != StatusCode::OK {
+            bail!("Responded with status code {status}");
+        }
+        if let Some(content_length) = self.headers().get(CONTENT_LENGTH) {
+            if content_length.to_str()?.parse::<u16>() == Ok(0) {
+                bail!("Responded with content-length equal to zero");
+            }
+        }
+
+        if self.is_html() {
+            bail!("Responded with HTML");
+        }
+        Ok(())
+    }
+}
--- src/webpage.rs
+++ src/webpage.rs
@@ -1,8 +1,6 @@
-use color_eyre::{eyre::bail, Result};
-use hyper::{
-    header::{CONTENT_LENGTH, CONTENT_TYPE},
-    Body, Response, StatusCode,
-};
+use color_eyre::Result;
+use hyper::Body;
+use hyper::Response;
 use soup::prelude::*;
 use url_path::UrlPath;
 
@@ -21,48 +19,6 @@ fn list_raw(text: &str) -> Vec<String> {
             None
         })
         .collect::<Vec<_>>()
-}
-
-/// Adds extra functionality to `hyper::Response<Body>`.
-pub trait ResponseExt {
-    /// Returns true if the response has a `Content-Type` header indicating it is HTML.
-    fn is_html(&self) -> bool;
-
-    /// Verifies that the response is valid according to various criteria.
-    fn verify(&self) -> Result<()>;
-}
-
-impl ResponseExt for Response<Body> {
-    /// Returns true if the response has a `Content-Type` header indicating it is HTML.
-    fn is_html(&self) -> bool {
-        self.headers()
-            .get(CONTENT_TYPE)
-            .map(|content_type| {
-                content_type
-                    .to_str()
-                    .map(|t| t == "text/html")
-                    .unwrap_or(false)
-            })
-            .unwrap_or(false)
-    }
-
-    /// Verifies that the response is valid according to various criteria.
-    fn verify(&self) -> Result<()> {
-        let status = self.status();
-        if status != StatusCode::OK {
-            bail!("Responded with status code {status}");
-        }
-        if let Some(content_length) = self.headers().get(CONTENT_LENGTH) {
-            if content_length.to_str()?.parse::<u16>() == Ok(0) {
-                bail!("Responded with content-length equal to zero");
-            }
-        }
-
-        if self.is_html() {
-            bail!("Responded with HTML");
-        }
-        Ok(())
-    }
 }
 
 /// Returns a list of files parsed from the HTML in a `hyper::Response<Body>`.
